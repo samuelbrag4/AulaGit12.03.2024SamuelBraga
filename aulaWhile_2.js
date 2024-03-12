860bdc9e46f937c571a9a3ca5b214119a9f49ea8
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-03-12 15:17:08 -0300

    nao entendi

---
+++ aulaWhile_2.js
@@ -0,0 +1,14 @@
+// Criei uma variavel inicial valendo 1.
+let inicio = 0;
+let fim = 10;
+
+// Realizo o looping while/enquanto.
+while (inicio <= fim) {
+    // Faco a minha logica aqui.
+    console.log(inicio);
+    inicio++;
+}
+
+// Depois que while, terminou. Ou seja, vai para a proxima
+// Linha
+console.log("terminou");
