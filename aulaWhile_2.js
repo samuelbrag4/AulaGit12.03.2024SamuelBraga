c335378225f728049c247dc5795a8f08b10633d2
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-03-12 16:41:17 -0300

    Criei uma cadeia de if para dar valor a inicio e fim, dessa forma o menor número será inicio e o maior fim

--- aulaWhile_2.js
+++ aulaWhile_2.js
@@ -4,6 +4,14 @@ let b = 1;
 let inicio = 0;
 let fim = 0;
 
+// Crio uma cadeia de if para dar valor a inicio e fim
+if (a >= b) {
+    inicio = b;
+    fim = a;
+} else {
+    inicio = a;
+    fim = b;
+}
 // Realizo o looping while/enquanto.
 while (inicio <= fim) {
     // Faco a minha logica aqui.
